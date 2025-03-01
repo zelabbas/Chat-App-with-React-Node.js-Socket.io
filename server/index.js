4a140a37bcae3261aba408c2ceb486b6e2f14b9c
zelabbas <zakariaelabbassi2002@gmail.com>
2025-03-01 17:49:43 +0000

    start build part server and install all his dependencies

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+// check if the connection in mognodb is successful
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+
+}).catch((err) => { 
+    console.log(err) 
+    console.log('Connection failed');
+});
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port: ${process.env.PORT}`);
+});
